28a486b612eb678c82af34b48cfb565c807b2f1e
JiSop <jisop.shin@gmail.com>
2019-12-13 18:18:11 +0900

    Auto stash before rebase of "refs/heads/master"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Tasks />
     </div>
   );
 }
